a363a2cd1edda7c7d86af920d899bd0816081e6b
AryanshRana <ranaaryansh12@gmail.com>
2023-12-03 13:44:10 +0530

    slicing in 1d and 2d array

---
+++ 2Dslicing.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr = np.array([["name ","of ","something ","we don't "],["things ","we ","loose ","and "]])
+print(arr[0:3,1:3])
---
+++ slicing.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr = np.array(["s","a","e","i","o","u","q","f","l","w"])
+print(arr[1:6:])
